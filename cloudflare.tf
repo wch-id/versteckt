fa6c7a3ab8bd20eac050a181dda81ab6f047a408
Saputro Aryulianto <aryuliantos@gmail.com>
2023-06-27 21:36:28 +0700

    Update cloudflare.tf

--- cloudflare.tf
+++ cloudflare.tf
@@ -88,7 +88,7 @@ resource "cloudflare_record" "terraform_managed_resource_dbbc4930417c98a66a37ad6
   zone_id = "276ca4158d4acf658b7f679805e747fb"
 }
 
-resource "cloudflare_record" "terraform_managed_resource_dbbc4930417c98a66a37ad68253c7fc6" {
+resource "cloudflare_record" "terraform_managed_resource_testing_pr" {
   name    = "testing-pr"
   proxied = false
   ttl     = 1
